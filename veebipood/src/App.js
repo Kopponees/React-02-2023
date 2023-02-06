ed7eb77b5bb9059d07d54a09b701971d72f26276
Kopponees <eilandliis@gmail.com>
2023-02-06 16:01:33 +0200

    Reacti uue projekti loomine

---
+++ veebipood/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://th.bing.com/th/id/R.e258b3e684127adafbdfcf680c7ce64d?rik=wsZtiR2zhs1GdQ&riu=http%3a%2f%2fcartype.com%2fpics%2f11525%2ffull%2fnobe_100_18_4.jpg&ehk=FTKoL5z2dUr36X6Hy0McetnzuqaRRYOzDXypqf0F1MI%3d&risl=&pid=ImgRaw&r=0" alt="" />
+   <button className="nupp">Nupp</button>
+    </div>
+  );
+}
+
+export default App;
