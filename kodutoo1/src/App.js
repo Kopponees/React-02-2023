bc0978fd4356c8040139bc0cfa17111b29390a4a
Kopponees <eilandliis@gmail.com>
2023-02-07 23:48:16 +0200

    Reacti kodutöö parandus

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -7,8 +7,9 @@ function App() {
       <button className="nupuke">Nupuke</button>
       <header className="tekst">tekst</header>
       <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/900px-Cat03.jpg?20160327035619" alt="" />
-
-    </div>
+</div>
+    
+    
   );
 }
 
