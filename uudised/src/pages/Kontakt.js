1e289e114c02dcd63d88bde8123e32fb37653501
Kopponees <eilandliis@gmail.com>
2023-02-27 09:41:24 +0200

    favicon, firebase, font

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -12,19 +12,19 @@ function Kontakt() {
         <div>Kontaktid</div>
         <div>Võta meiega ühendust!</div>
         <br />
-        <div  className="vajutatavTekst" onClick={() => muudaN2itaTelVigala(true)}>Vana-Vigala Tehnika-ja Teeninduskool</div>
-        {n2itaTelVigala && <div>+37258674355</div>}
+        <div  className={n2itaTelVigala === true ? "valitud" : undefined} onClick={() => muudaN2itaTelVigala(true)}>Vana-Vigala Tehnika-ja Teeninduskool <br />
+        {n2itaTelVigala && +37258674355}</div>
         <div>Hariduse tn 2, Vana-Vigala, Raplamaa</div>
         { email  !=="" &&<div>Kooli email: {email}</div>} 
         <button className="emailnupp" onClick={() =>n2itaEmail("sepad@sepad.ee")}>kirjuta meile</button>
         <br />
         <br />
         <div>Juhatuse esimehe konkakt </div> 
-        <div className="vajutatavTekst" onClick={() => muudaN2itaTelKiri(true)}> Joonas Kiri {n2itaTelKiri && +3725435666} Kiri@sepad.ee</div>
+        <div className={n2itaTelKiri === true ? "valitud" : undefined} onClick={() => muudaN2itaTelKiri(true)}> Joonas Kiri  <br />{n2itaTelKiri && +3725435666} <br /> Kiri@sepad.ee</div>
         <br />
         <div>Ühenduse liikmete kontaktid:</div>
-        <div className="vajutatavTekst" onClick={() => muudaN2itaTelSurva(true)}>Olav Surva {n2itaTelSurva &&+3725640403}</div> 
-        <div className="vajutatavTekst" onClick ={() => muudaN2itaTelReeman(true)}>Peeter Reeman {n2itaTelReeman &&+372494959}</div>
+        <div className={n2itaTelSurva === true ? "valitud" : undefined} onClick={() => muudaN2itaTelSurva(true)}>Olav Surva {n2itaTelSurva &&+3725640403}</div> 
+        <div className={n2itaTelReeman === true ? "valitud" : undefined} onClick ={() => muudaN2itaTelReeman(true)}>Peeter Reeman {n2itaTelReeman &&+372494959}</div>
         
 
     </div>
