eaa2828f4f38934b0b875f13c8c154f6f75337eb
Kopponees <eilandliis@gmail.com>
2023-03-03 18:37:45 +0200

    array, kustutamine, filtreerimine, sorteerimine

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Ilmumine from './pages/Ilmumine';
 import Kujundus from './pages/Kujundus';
 import Muutmine from './pages/Muutmine';
+import Array from './pages/Array';
 
 function App() {
   return (
@@ -11,12 +12,14 @@ function App() {
       <Link to="/ilmumine"><button>Ilmumine</button></Link>
       <Link to="/kujundus"><button>Kujundus</button></Link>
       <Link to="/muutmine"><button>Muutmine</button></Link>
+      <Link to="/array"><button>Array</button></Link>
 
      <Routes>
       <Route path="/" element={<Navigate to="/ilmumine" />} />
       <Route path="/ilmumine" element={ <Ilmumine /> } />
       <Route path="/kujundus" element={ <Kujundus /> } />
       <Route path="/muutmine" element={ <Muutmine /> } />
+      <Route path="/array" element={ <Array /> } />
      </Routes>
     </div>
   );
---
+++ usestate/src/pages/Array.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import muutujadFailist from '../muutujad.json';
+
+
+function Array() {
+
+    const[muutuja, muudaMuutujat] = useState(muutujadFailist);
+
+   
+
+    
+ 
+    function kustuta(index) {
+        muutuja.splice(index,1);
+        muudaMuutujat(muutuja.slice());
+    }    
+
+
+    function kustutaMuutujad() {
+        muudaMuutujat([]);}
+
+        const sorteeriAZ= () => {
+            muutuja.sort((a,b) => a.localeCompare(b));
+            muudaMuutujat(muutuja.slice());
+        }
+
+        const filtreeriRohkemKui4Tahelised= () => {
+            const tulem= muutuja.filter(element => element.length > 4);
+            muudaMuutujat(tulem);
+        }
+
+    
+ 
+    return (
+    <div>
+        <button onClick={kustutaMuutujad}>kustuta kõik</button>
+        <br />
+        <button onClick={sorteeriAZ}>sorteeri A-Z</button>
+        <button onClick={filtreeriRohkemKui4Tahelised}>Jäta alles üle 4 tähelised</button>
+        <div>{muutuja.length} tk</div> 
+
+
+
+
+        
+        {muutuja.map((element, index) =>
+        <div>
+            <span>{element}</span>
+            <button onClick={() => kustuta(index)}>X</button>
+        </div>)}
+    </div>
+  )
+}
+
+export default Array
