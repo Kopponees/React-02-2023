2fc95b0793bfa56e071c4d4a8f6f0c3e0bdc40db
Kopponees <eilandliis@gmail.com>
2023-03-14 21:22:24 +0200

    uue lehe alustus, impordid

---
+++ minuleht/src/App.js
@@ -0,0 +1,16 @@
+import { Link, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Avaleht from '../pages/Avaleht';
+import Galerii from '../pages/Galerii';
+import Kontakt from '../pages/Kontakt';
+import Meist from '../pages/Meist';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
