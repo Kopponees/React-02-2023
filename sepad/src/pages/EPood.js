3e41be728c15e84e4fa40ef662264c62afc92c9d
Kopponees <eilandliis@gmail.com>
2023-04-18 14:49:42 +0300

    epood, bootstrap

--- sepad/src/pages/EPood.js
+++ sepad/src/pages/EPood.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import KaubadFailist from '../data/kaubad.json';
 import "../css/Ostukorv.css";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function EPood() {
   const [toode]= useState(KaubadFailist);
@@ -14,23 +18,48 @@ function EPood() {
       cartLS.push({ kaup: klikitudToode, quantity: 1 });
     }}
     
-
+   
   return (
-    <div>
+    
+    <>
+      <div className="mb-2">
+        {[ 'end',].map(
+          (direction) => (
+            <DropdownButton
+              as={ButtonGroup}
+              key={direction}
+              id={`dropdown-button-drop-${direction}`}
+              drop={direction}
+              variant="dark"
+              title="tooted"
+            >
+              <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+              <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </DropdownButton>
+          ),
+        )}
+      </div>
+      <div className="product-wrapper-home">
       {toode.map(element=>
-        <div className="kaubad">
+        <div className="product-home">
           <img className="epoodimage" src={element.image} alt="" />
           <div className="epoodtekst">
             <div className="epoodname">{element.name}</div>
           <div className="epoodprice">{element.price}€</div>
           <div className="epoodblacksmith">{element.blacksmith}</div>
-          <button onClick={lisaOstukorvi}>lisa korvi</button>
+          <Button variant="outline-dark" onClick={lisaOstukorvi}>lisa korvi</Button>
           </div>
           
         </div>
         )}
     </div>
-  )
+      
+    </>
+  );
+   
 }
 
 export default EPood
--- sepad/src/pages/Ostukorv.js
+++ sepad/src/pages/Ostukorv.js
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+import { useContext } from 'react';
+import { CartSumContext } from '../../store/CartSumContext';
+import ParcelMachine from '../../components/cart/ParcelMachine';
+import Payment from '../../components/cart/Payment';
 
 function Ostukorv() {
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || []);
+  const { setCartSum } = useContext(CartSumContext);
+
+  const decreaseQuantity = (index) => {
+    cart[index].quantity = cart[index].quantity - 1;
+    if (cart[index].quantity === 0) {
+      deleteProduct(index);
+    }
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart));
+    setCartSum(calculatetotalValue());
+
+  }
+  const increaseQuantity = (index) => {
+    cart[index].quantity = cart[index].quantity + 1;
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart));
+    setCartSum(calculatetotalValue());
+  }
+
+  const deleteProduct = (index) => {
+    cart.splice(index, 1);
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart));
+    setCartSum(calculatetotalValue());
+  }
+
+  const empty = () => {
+    setCart([]);
+    localStorage.setItem("cart", JSON.stringify([]));
+    setCartSum("0.00");
+  }
+  const calculatetotalValue = () => {
+    let totalValue = 0;
+    cart.forEach(oneProduct => totalValue = totalValue + oneProduct.product.price * oneProduct.quantity);
+    return totalValue.toFixed(2);
+  }
+  
   return (
-    <div>Ostukorv</div>
+    <div>
+      <div className="cart-top">
+        {cart.length > 0 && <Button variant="outlined" onClick={empty}>Empty</Button>}
+        {cart.length === 0 && <div>Cart is empty! <Link to="/">Add products</Link></div>}
+        {cart.length > 0 && <div> Total of {cart.length} products in cart!</div>}
+      </div>
+
+      {cart.map((element, index) =>
+      <div className="product-wrapper">
+        <div className="product" key={index}>
+          <img className="image" src={element.product.image} alt="" />
+          <div className="name">{element.product.name}</div>
+          <div className="price">{element.product.price} €</div>
+          <div className="quantity">
+            <img className="button" onClick={() => decreaseQuantity(index)} src="minus.png" alt="" />
+            <div>{element.quantity}</div>
+            <img className="button" onClick={() => increaseQuantity(index)} src="add.png" alt="" />
+          </div>
+          <div className="sum">{(element.product.price * element.quantity).toFixed(2)} €</div>
+          <img className="button" onClick={() => deleteProduct(index)} src="trash.png" alt="" />
+      </div>
+          <div className="mobile-row">
+            <img className="button" onClick={() => decreaseQuantity(index)} src="minus.png" alt="" />
+            <div>{element.quantity}</div>
+            <img className="button" onClick={() => increaseQuantity(index)} src="add.png" alt="" />
+          </div>
+        </div>
+      )}
+      {cart.length > 0 &&
+        <div className="cart-bottom">
+          <div> Total Value: {calculatetotalValue()} €</div>
+          <ParcelMachine />
+          <Payment sum={calculatetotalValue()} empty={empty} />
+        </div>}
+    </div>
   )
 }
 
