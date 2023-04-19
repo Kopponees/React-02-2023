7daa2cf99b34ef8f7b459ff104a6f3a70728f4ee
Kopponees <eilandliis@gmail.com>
2023-04-19 22:47:36 +0300

    dropdownbuttons

--- sepad/src/pages/EPood.js
+++ sepad/src/pages/EPood.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function EPood() {
-  const [product]= useState(KaubadFailist);
+  const [product] = useState(KaubadFailist);
 
   const lisaOstukorvi = (productClicked) => {
     const cartLS = JSON.parse(localStorage.getItem("cart")) || [];
@@ -19,49 +19,57 @@ function EPood() {
     }
     localStorage.setItem("cart", JSON.stringify(cartLS));
   };
-    
-   
+
+  const blacksmiths = [...new Set(product.map((item) => item.blacksmith))];
+
   return (
-    
     <>
       <div className="mb-2">
-        {[ 'end',].map(
-          (direction) => (
-            <DropdownButton
-              as={ButtonGroup}
-              key={direction}
-              id={`dropdown-button-drop-${direction}`}
-              drop={direction}
-              variant="dark"
-              title="tooted"
-            >
-              <Dropdown.Item eventKey="1"></Dropdown.Item>
-              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-              <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-            </DropdownButton>
-          ),
-        )}
+        <DropdownButton className="sepadnuppepood"
+          as={ButtonGroup}
+          id="dropdown-button-drop-blacksmith"
+          variant="dark"
+          title="Sepp"
+        >
+          {blacksmiths.map((blacksmith) => (
+            <Dropdown.Item key={blacksmith} eventKey={blacksmith}>
+              {blacksmith}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <DropdownButton className="toodenuppepood"
+          as={ButtonGroup}
+          id="dropdown-button-drop-name"
+          variant="dark"
+          title="Toode"
+        >
+          {product.map((item) => (
+            <Dropdown.Item key={item.id} eventKey={item.name}>
+              {item.name}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
       </div>
       <div className="product-wrapper-home">
-      {product.map(element=>
-        <div className="product-home">
-          <img className="epoodimage" src={element.image} alt="" />
-          <div className="epoodtekst">
-            <div className="epoodname">{element.name}</div>
-          <div className="epoodprice">{element.price}€</div>
-          <div className="epoodblacksmith">{element.blacksmith}</div>
-          <Button variant="outline-dark" onClick={() => lisaOstukorvi(element)}>lisa korvi</Button>
+        {product.map((element) => (
+          <div className="product-home" key={element.id}>
+            <img className="epoodimage" src={element.image} alt="" />
+            <div className="epoodtekst">
+              <div className="epoodname">{element.name}</div>
+              <div className="epoodprice">{element.price}€</div>
+              <div className="epoodblacksmith">{element.blacksmith}</div>
+              <Button
+                variant="outline-dark"
+                onClick={() => lisaOstukorvi(element)}
+              >
+                lisa korvi
+              </Button>
+            </div>
           </div>
-          
-        </div>
-        )}
-    </div>
-      
+        ))}
+      </div>
     </>
   );
-   
 }
 
-export default EPood
+export default EPood;
